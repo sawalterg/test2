92c0c7fbb38926615c03baf4d500b03ae55bc723
sawalterg <38083974+sawalterg@users.noreply.github.com>
2018-05-03 09:03:44 -0400

    Create blah.py

---
+++ blah.py
@@ -0,0 +1,12 @@
+#! python3
+# test2.py - a file that tests stuff
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+newVarDict = {}
+newVarList []
+
+newVarArray = np.array(newVarList)
